cc7f9323e60cd182ae475d86560984eb3036e858
SibiSM <60312989+SibiSM@users.noreply.github.com>
2024-07-09 19:53:10 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const apiUrl = 'http://localhost:8000/api';
+    const apiUrl = 'http://localhost:8080/api';
 
     // Register Form Submission
     const registerForm = document.getElementById('registerForm');
